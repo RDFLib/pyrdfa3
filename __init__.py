eb5dc4c34b2817a0441a8a6cedcf248e28e45cdd
Ivan Herman <ivan@ivan-herman.net>
2012-03-08 15:08:28 +0000

    *** empty log message ***

--- __init__.py
+++ __init__.py
@@ -159,7 +159,7 @@ U{W3C® SOFTWARE NOTICE AND LICENSE<href="http://www.w3.org/Consortium/Legal/200
 """
 
 """
-$Id: __init__.py,v 1.62 2012-03-08 10:54:50 ivan Exp $ $Date: 2012-03-08 10:54:50 $
+$Id: __init__.py,v 1.63 2012-03-08 15:08:28 ivan Exp $ $Date: 2012-03-08 15:08:28 $
 
 Thanks to Victor Andrée, who found some intricate bugs, and provided fixes, in the interplay between @prefix and @vocab...
 
@@ -798,7 +798,6 @@ def processURI(uri, outputFormat, form={}) :
 			retval = 'Content-Type: application/json; charset=utf-8\n'
 		else :
 			retval = 'Content-Type: application/rdf+xml; charset=utf-8\n'
-		retval += 'Status: %s\n\n' % processor.http_status
 		
 		retval += graph
 		return retval
