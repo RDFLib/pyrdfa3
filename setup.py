9d29d12ef0d17f9afbded8f912511006c571cb1d
Ivan Herman <ivan@ivan-herman.net>
2012-08-26 06:56:50 +0200

    The list of packages listed in setup did not correctly reflect the separation of 'core' and the pyRdfaExtra package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name="pyRdfa",
       version="3.4.2",
       author="Ivan Herman",
       author_email="ivan@w3.org",
-	  maintainer="Ivan Herman",
-	  maintainer_email="ivan@w3.org",
-      packages=['pyRdfa','pyRdfa.serializers','pyRdfa.transform','pyRdfa.extras','pyRdfa.rdfs','pyRdfa.host','pyRdfaExtras'])
+      maintainer="Ivan Herman",
+      maintainer_email="ivan@w3.org",
+      packages=['pyRdfa','pyRdfa.transform','pyRdfa.extras','pyRdfa.rdfs','pyRdfa.host','pyRdfaExtras',pyRdfaExtras.extras,pyRdfaExtras.serializers])
 
