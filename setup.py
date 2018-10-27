fb1b67c2d990f73d6ffbc81db4864e7fcc0b109a
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2018-10-27 22:27:03 +0200

    scaffolding to work as a rdflib plugin
    copied RDFLib specific parser classes to rdflibparsers.py
    entry_points for registering these.

--- setup.py
+++ setup.py
@@ -21,5 +21,19 @@ setup(name="pyRdfa",
 				'pyRdfaExtras',
 				'pyRdfaExtras.extras',
 				'pyRdfaExtras.serializers'
-				])
+				],
+    entry_points={
+        'rdf.plugins.parser': [
+            'rdfa = pyRdfa.rdflibparsers:RDFaParser',
+            'rdfa1.0 = pyRdfa.rdflibparsers:RDFa10Parser',
+            'rdfa1.1 = pyRdfa.rdflibparsers:RDFaParser',
+            'application/svg+xml = pyRdfa.rdflibparsers:RDFaParser',
+            'application/xhtml+xml = pyRdfa.rdflibparsers:RDFaParser',
+            'hturtle = pyRdfa.rdflibparsers:HTurtleParser'
+            'html = pyRdfa.rdflibparsers:StructuredDataParser'
+            'html/text = pyRdfa.rdflibparsers:StructuredDataParser'
 
+        ],
+    }
+
+)
